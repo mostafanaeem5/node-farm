e102440ed1767c8adda7195f88b3dc9ddb4e90f3
mostafanaeem5 <moustafa.31039842@ics.tanta.edu.eg>
2023-07-22 14:59:43 +0300

    made a simple api

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const hello = 'Hello world!'
-console.log(hello);
+const http = require("http")
+const url = require('url')
+const fs = require('fs')
+const data = fs.readFileSync("./dev-data/data.json","utf-8")
+console.log(data);
+http.createServer((req,res) => {
+   const pathName = req.url
+    if(pathName === '/'){
+        res.end('Welcome!')
+    }else if(pathName === '/overview'){
+        res.end('welcome to the overview!')
+    }else if(pathName === '/api'){
+        res.writeHead(200,{'Content-type':'application/json'})
+        JSON.parse(data)
+        res.end(data)
+        
+    }
+    res.end('hello world')
+}).listen(3000)
+
